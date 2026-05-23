8457ad79914b3a31033bdc2f31d9f8874e3ce331
Himanshu Choudhary <himanshuchoudhary9365@gmail.com>
2026-05-23 18:03:59 +0530

    Login Page and Signup Page Completed

--- lib/core/configs/theme/app_theme.dart
+++ lib/core/configs/theme/app_theme.dart
@@ -7,6 +7,27 @@ class AppTheme {
     scaffoldBackgroundColor: AppColors.lightBackground,
     brightness: Brightness.light,
     fontFamily: 'Satoshi',
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: const Color(0xffF2F2F2),
+      hintStyle: const TextStyle(
+        color: Color(0xff383838),
+        fontWeight: FontWeight.w500,
+      ),
+      contentPadding: const EdgeInsets.all(30),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Color(0xffD6D6D6), width: 1),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Color(0xffD6D6D6), width: 1),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: AppColors.primary, width: 1.5),
+      ),
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
@@ -22,6 +43,27 @@ class AppTheme {
     scaffoldBackgroundColor: AppColors.darkBackground,
     brightness: Brightness.dark,
     fontFamily: 'Satoshi',
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: const Color(0xff1A1A1A),
+      hintStyle: const TextStyle(
+        color: Color(0xffA7A7A7),
+        fontWeight: FontWeight.w500,
+      ),
+      contentPadding: const EdgeInsets.all(30),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.white24, width: 1),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.white24, width: 1),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: AppColors.primary, width: 1.5),
+      ),
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
---
+++ lib/presentation/auth/pages/signup.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify_clone_with_flutter/common/widgets/appbar/app_bar.dart';
+import 'package:spotify_clone_with_flutter/common/widgets/button/basic_app_button.dart';
+import 'package:spotify_clone_with_flutter/core/configs/assets/app_vectors.dart';
+import 'package:spotify_clone_with_flutter/presentation/auth/pages/signin.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: _siginText(context),
+      appBar: BasicAppbar(
+        title: SvgPicture.asset(AppVectors.logo, height: 40, width: 30),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _registerText(),
+            const SizedBox(height: 50),
+            _fullNameField(context),
+            const SizedBox(height: 20),
+            _emailField(context),
+            const SizedBox(height: 20),
+            _passwordField(context),
+            const SizedBox(height: 20),
+            BasicAppButton(onPressed: () {}, title: 'Create Account'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _registerText() {
+    return const Text(
+      'Register',
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _fullNameField(BuildContext context) {
+    return const TextField(decoration: InputDecoration(hintText: 'Full Name'));
+  }
+
+  Widget _emailField(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(hintText: 'Enter Email'),
+    );
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return const TextField(decoration: InputDecoration(hintText: 'Password'));
+  }
+
+  Widget _siginText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Do You Have An Account?',
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => const SigninPage(),
+                ),
+              );
+            },
+            child: Text(
+              'Sign In',
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
